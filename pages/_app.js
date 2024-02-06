44a86c60f06f5ff5d6d02b4b610655346ec87020
Hobermac <54035686+Anton1636@users.noreply.github.com>
2024-02-06 22:06:01 +0200

    Added frontend part

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import '@/styles/globals.css'
-import { TrackingProvider } from '../context/tracking'
+import { TrackingProvider } from '../context/Tracking'
+import { NavBar, Footer } from '../components'
 
 export default function App({ Component, pageProps }) {
 	return (
 		<>
 			<TrackingProvider>
+				<NavBar />
 				<Component {...pageProps} />
 			</TrackingProvider>
+			<Footer />
 		</>
 	)
 }
